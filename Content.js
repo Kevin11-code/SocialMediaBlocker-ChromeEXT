87e6c4aac830780608c1ac1116a1b78e7a45537e
kevin11-code <kevinharia11@gmail.com>
2023-04-11 11:54:39 +0530

    init commit

--- Content.js
+++ Content.js
@@ -251,29 +251,37 @@ const generateHTML = (pageName) => {
    `;
 };
 
-switch (window.location.hostname) {
-  case "www.youtube.com":
+const blacklist = [/^.*v3cars.*$/, /^.*netflix.*$/];
+
+for (let i = 0; i < blacklist.length; i++) {
+  // if (window.location.hostname.includes(blacklist[i])) {
+  if (blacklist[i].test(window.location.hostname)) {
     document.head.innerHTML = generateSTYLES();
-    document.body.innerHTML = generateHTML("YOUTUBE");
-    break;
-  case "www.facebook.com":
-    document.head.innerHTML = generateSTYLES();
-    document.body.innerHTML = generateHTML("FACEBOOK");
-    break;
-  case "www.netflix.com":
-    document.head.innerHTML = generateSTYLES();
-    document.body.innerHTML = generateHTML("NETFLIX");
-    break;
-  case "www.roblox.com":
-    document.head.innerHTML = generateSTYLES();
-    document.body.innerHTML = generateHTML("ROBLOX");
-    break;
-  case "discord.com":
-    document.head.innerHTML = generateSTYLES();
-    document.body.innerHTML = generateHTML("DISCORD");
-    break;
-  case "www.spotify.com":
-    document.head.innerHTML = generateSTYLES();
-    document.body.innerHTML = generateHTML("SPOTIFY");
-    break;
+    document.body.innerHTML = generateHTML("Crytptojacking Therat");
+  }
+}
+if (window.location.hostname.endsWith("www.youtube.com")) {
+  document.head.innerHTML = generateSTYLES();
+  document.body.innerHTML = generateHTML("YOUTUBE");
+  //   break;
+  // case "www.v3cars.com":
+  //   document.head.innerHTML = generateSTYLES();
+  //   document.body.innerHTML = generateHTML("FACEBOOK");
+  //   break;
+  // case "www.netflix.com":
+  //   document.head.innerHTML = generateSTYLES();
+  //   document.body.innerHTML = generateHTML("NETFLIX");
+  //   break;
+  // case "www.roblox.com":
+  //   document.head.innerHTML = generateSTYLES();
+  //   document.body.innerHTML = generateHTML("ROBLOX");
+  //   break;
+  // case "discord.com":
+  //   document.head.innerHTML = generateSTYLES();
+  //   document.body.innerHTML = generateHTML("DISCORD");
+  //   break;
+  // case "www.spotify.com":
+  //   document.head.innerHTML = generateSTYLES();
+  //   document.body.innerHTML = generateHTML("SPOTIFY");
+  //   break;
 }
